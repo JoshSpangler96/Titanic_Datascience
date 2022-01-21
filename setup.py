49e6d6a084e3582f89e3bb7b8391a40e0788ed0a
Spangler <Joshua.Spangler@viasat.com>
2022-01-21 10:57:35 -0800

    Predicted survival from logistic regression

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from distutils.core import setup
 
 setup(
     install_requires=[
-        'pypandoc>=1.4'
+        'pypandoc>=1.4',
+        'watermark>=2.3.0',
+        'pandas>=1.3.5',
+        'scikit-learn>=1.0.2',
+        'scipy>=1.7.3',
+        'matplotlib>=3.5.1'
     ]
 
 )
